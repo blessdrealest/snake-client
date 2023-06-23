dc18a2ccd33426a29957b2bbaf6bbae93791bc32
Blessing <dijiblessing2@hotmail.co.uk>
2023-06-23 00:01:28 -0600

    refactored code for eexport of input function into play.js

--- play.js
+++ play.js
@@ -1,27 +1,6 @@
 const net = require("net");
 const { connect } = require("./client");
-
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-
-
-
-
-};
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  
-  stdin.on("data", handleUserInput);
-  
-  return stdin;
-
-};
+const { setupInput } = require("./input");
 
 
 
@@ -30,4 +9,5 @@ const setupInput = function () {
 
 console.log("Connecting ...");
 connect();
+
 setupInput();
