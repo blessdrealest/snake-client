66ac18c14e20a1e0c41158e75e59ce2727a834aa
Blessing <dijiblessing2@hotmail.co.uk>
2023-06-22 22:21:30 -0600

    added net defined. Improved code to allow for export and import of connect function using require.

--- playUp.js
+++ playUp.js
@@ -1,21 +1,8 @@
 const net = require("net");
+const { connect } = require("./client");
+
 
 //establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '172.28.47.225',
-    port: 50541
-  });
-
-  conn.on("data", (data) => {
-    console.log("Server says:", data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
