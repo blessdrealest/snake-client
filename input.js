67143f8fbc597c51e2acfe5ad5ebbbf5a82ab256
Blessing <dijiblessing2@hotmail.co.uk>
2023-06-23 21:51:02 -0600

    updated connection object as well as inclusion of movement keys. Currently failing tests. Code refactoring required

--- input.js
+++ input.js
@@ -1,14 +1,36 @@
+ 
+ 
+ //stores the active TCP connection object
+ let connection;
+
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+
+  if (key === MOVE_UP_KEY) {
+    connection.write('Move: up');
+  }
+  if (key === MOVE_LEFT_KEY) {
+    connection.write('Move: left');
+  }
+  if (key === MOVE_DOWN_KEY) {
+    connection.write('Move: down');
+  }
+  if (key === MOVE_RIGHT_KEY) {
+    connection.write('Move: right');
+  }
+
+ 
+
 
 
 
 
 
 };
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- playUp.js
+++
@@ -1,8 +0,0 @@
-const net = require("net");
-const { connect } = require("./client");
-
-
-//establishes a connection with the game server
-
-console.log("Connecting ...");
-connect();
